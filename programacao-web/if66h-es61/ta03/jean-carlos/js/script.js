cc0adfcdfd8bbdfa24c3597b3caf93f99b5f019a
JeanCG7 <jean.goncalves7@etec.sp.gov.br>
2018-09-19 21:52:25 -0300

    Adicionando estilização

--- programacao-web/if66h-es61/ta03/jean-carlos/js/script.js
+++ programacao-web/if66h-es61/ta03/jean-carlos/js/script.js
@@ -3,10 +3,8 @@ $(document).ready(() => {
         type: 'GET',
         url: 'https://diogocezar.github.io/bazar/json/database.json',
         success: function (data) {
-            config = data.configs;
-
-            $('#about').append(aboutContent(config));
-            debugger;
+            $('#about').append(aboutContent(data.configs));            
+            $('#products').append(productsContent(data.products));            
 
         },
     });
@@ -15,16 +13,32 @@ $(document).ready(() => {
 
 
 function aboutContent(config) {
-    var content;
+    var content='<div class="about">';
     content += '<h1>' + config.title + '</h1>';
     content += '<h2>' + config.subTitle + '</h2>';
     content += '<p>' + config.email + '</p>';
     content += '<p>' + config.phone + '</p>';
-    content += '<p>' + config.email + '</p>';
     config.description.forEach(d => {
         $.each(d, function (key, value) {
             content += '<'+key+'>'+value+'</'+key+'>';
         });
     });
+    content+='</div>'
+    return content;
+}
+
+function productsContent(products){
+    var content='<div class="products">';
+    products.forEach(p =>{
+        content+='<h2>'+p.name+'</h2>';
+        content+='<img src="'+p.image+'"</img>';
+        p.description.forEach(d =>{
+            $.each(d, function(key, value){
+                content+='<'+key+'>'+value+'</'+key+'>';
+            });
+        });    
+        content+='<h2>R$ '+p.price+',00</h2>';
+    });
+    content+='</div>'
     return content;
 }
