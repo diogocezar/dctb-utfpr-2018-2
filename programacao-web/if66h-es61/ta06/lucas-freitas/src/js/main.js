6fc49f015c747807e99098b8f25deabcd896b82f
Lucas <lucas.freitascosta96@gmail.com>
2018-10-17 18:40:02 -0300

    ,(º :)

---
+++ programacao-web/if66h-es61/ta06/lucas-freitas/src/js/main.js
@@ -0,0 +1,8 @@
+function act() {
+if(document.getElementById("gulp").innerHTML == "Vamos la gulp!") {
+    document.getElementById("gulp").innerHTML = "A Falta de Criatividade é prejudicial à Felicidade";
+}
+else{
+    document.getElementById("gulp").innerHTML = "Vamos la gulp!";
+}
+}
