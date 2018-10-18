ec1322926837120c4537977551684da9b620a9ed
luctaru <lucastarumoto@gmail.com>
2018-10-18 13:11:35 -0300

    TA06 correcao

--- programacao-web/if66h-es61/ta06/lucas-tarumoto/gulpfile.js
+++ programacao-web/if66h-es61/ta06/lucas-tarumoto/gulpfile.js
@@ -6,27 +6,22 @@ var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
  
 var minify = composer(uglifyjs, console);
- 
-gulp.task('compress', function (cb) {
-  var options = {};
- 
-  pump([
-      gulp.src('lib/*.js'),
-      minify(options),
-      gulp.dest('dist')
-    ],
-    cb
-  );
-});
 
 gulp.task('default', function () {
-    gulp.src('src/**/*.css')
+    gulp.src('./*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist'));
-});
+        .pipe(gulp.dest('./dist'));
 
-gulp.task('watch', function(){
-    gulp.watch('./js/*.js');
-    gulp.watch('./css/*.css');
+        var options = {};
+ 
+  pump(
+      gulp.src('./js/*.js'),
+      minify(options),
+      gulp.dest('./dist'));
+
+
+        gulp.watch('./js/*.js');
+        gulp.watch('./*.css');
+
 });
